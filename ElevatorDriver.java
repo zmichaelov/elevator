40fc3ec2ae9750cb6f5507012688b45107469a93
Zachary Michaelov <zach.michaelov@gmail.com>
2013-11-10 15:50:45 -0500

    added extra riders

--- ElevatorDriver.java
+++ ElevatorDriver.java
@@ -7,10 +7,14 @@ public class ElevatorDriver {
         Thread rider2 = new Thread(new Rider(building, 1, 3), "B");
         Thread rider3 = new Thread(new Rider(building, 4, 0), "C");
         Thread rider4 = new Thread(new Rider(building, 2, 4), "D");
+        Thread rider5 = new Thread(new Rider(building, 3, 4), "E");
+        Thread rider6 = new Thread(new Rider(building, 3, 2), "F");
 
         rider1.start();
         rider2.start();
         rider3.start();
         rider4.start();
+        rider5.start();
+        rider6.start();
     }
 }
